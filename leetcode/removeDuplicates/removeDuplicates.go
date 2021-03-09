71d1bfc0f7c2deef392cf17fe204453fa661ec50
wanglei <cn_wlei@163.com>
2021-03-09 10:17:38 +0800

    1047. 删除字符串中的所有相邻重复项(用切片模拟栈)

---
+++ leetcode/removeDuplicates/removeDuplicates.go
@@ -0,0 +1,38 @@
+package main
+
+/**
+给出由小写字母组成的字符串S，重复项删除操作会选择两个相邻且相同的字母，并删除它们。
+在 S 上反复执行重复项删除操作，直到无法继续删除。
+在完成所有重复项删除操作后返回最终的字符串。答案保证唯一。
+
+示例：
+输入："abbaca"
+输出："ca"
+解释：
+例如，在 "abbaca" 中，我们可以删除 "bb" 由于两字母相邻且相同，这是此时唯一可以执行删除操作的重复项。之后我们得到字符串 "aaca"，其中又只有 "aa" 可以执行重复项删除操作，所以最后的字符串为 "ca"。
+
+提示：
+1 <= S.length <= 20000
+S 仅由小写英文字母组成。
+
+*/
+/**
+用切片模拟栈
+*/
+func removeDuplicates(S string) string {
+	var res []byte
+	size := 0
+	for i := 0; i < len(S); i++ {
+		if size != 0 && S[i] == res[size-1] {
+			size--
+			res = append(res[:size])
+		} else {
+			res = append(res, S[i])
+			size++
+		}
+	}
+	return string(res)
+}
+func main() {
+	removeDuplicates("abbaca")
+}
