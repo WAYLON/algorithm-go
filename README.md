e2d11f76f4dc691a7f9b165e4dca5fe96c5437cb
waylon <cn_wlei@163.com>
2021-09-01 13:57:37 +0800

    304. 二维区域和检索 - 矩阵不可变(前缀和)

--- README.md
+++ README.md
@@ -81,8 +81,6 @@
 
 ### `longestPalindromeSubstring 5. 最长回文子串`
 
-### `NumMatrix 304. 二维区域和检索 - 矩阵不可变`
-
 ### [`maxSumSubmatrix 363. 矩形区域不超过 K 的最大数值和(朴素解法)`](./leetcode/maxSumSubmatrix/maxSumSubmatrix.go)
 
 ### `NumMatrix 338. 比特位计数`
@@ -250,6 +248,8 @@
 ### [`corpFlightBookings 1109. 航班预订统计(前缀和)`](./leetcode/corpFlightBookings/corpFlightBookings.go)
 
 ### [`NumArray 303. 区域和检索 - 数组不可变(前缀和)`](./leetcode/NumArray/NumArray.go)
+
+### [`NumMatrix 304. 二维区域和检索 - 矩阵不可变(前缀和)`](./leetcode/NumMatrix/NumMatrix.go)
 
 ### [`countTriplets 1442. 形成两个异或相等数组的三元组数目(异或)`](./leetcode/countTriplets/countTriplets.go)
 
--- leetcode/NumMatrix/NumMatrix.go
+++ leetcode/NumMatrix/NumMatrix.go
@@ -52,6 +52,34 @@ func (this *NumMatrix) SumRegion(row1 int, col1 int, row2 int, col2 int) int {
 }
 
 /**
+
+java
+
+class NumMatrix {
+
+    private int[][] sums;
+
+    public NumMatrix(int[][] matrix) {
+        int n = matrix.length;
+        if (n > 0) {
+            int m = matrix[0].length;
+            sums = new int[n + 1][m + 1];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    sums[i+1][j+1] = sums[i][j+1] + sums[i+1][j] - sums[i][j] + matrix[i][j];
+                }
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return sums[row2 + 1][col2 + 1] - sums[row2 + 1][col1] - sums[row1][col2 + 1] + sums[row1][col1];
+    }
+}
+
+*/
+
+/**
  * Your NumMatrix object will be instantiated and called as such:
  * obj := Constructor(matrix);
  * param_1 := obj.SumRegion(row1,col1,row2,col2);
